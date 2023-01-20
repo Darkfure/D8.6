fc564f40dd6708fd480fabea6ae8dcbfd0216713
Darkfure <lion4652@yandex.ru>
2023-01-20 19:31:52 +0400

    Homework

---
+++ NewsPaper/news/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import PostsList, PostDetail, PostSearch, PostCreate, PostUpdate, PostDelete
+
+urlpatterns = [
+   path('', PostsList.as_view(), name='post_list'),
+   path('<int:pk>', PostDetail.as_view(), name='post_detail'),
+   path('search', PostSearch.as_view()),
+   path('news/create', PostCreate.as_view(), name='news_create'),
+   path('articles/create', PostCreate.as_view(), name='articles_create'),
+   path('news/<int:pk>/update', PostUpdate.as_view(), name='news_update'),
+   path('articles/<int:pk>/update', PostUpdate.as_view(), name='articles_update'),
+   path('news/<int:pk>/delete', PostDelete.as_view(), name='news_delete'),
+   path('articles/<int:pk>/delete', PostDelete.as_view(), name='articles_delete'),
+
+
+]
